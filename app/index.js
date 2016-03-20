b2d3333c0f7260aa4dd51880af4e11d4c1bf7b57
Mrugesh Mohapatra <me@raisedadead.com>
2016-03-20 17:47:48 +0530

    Add motivations

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const motivations = require('motivations');
 
 app.get('/', function(request, response){
 	response.send("Hello Bengaluru!");
@@ -7,6 +8,12 @@ app.get('/', function(request, response){
 
 app.get('/helloWorld', function(request, response){
 	response.send("Hello World!");
+});
+
+app.get('/Q_SelfRand', function(request, response){
+	var quote = motivations[Math.floor(Math.random()*motivations.length)];
+	response.send(quote);
+	console.log("Sent the Quote : " + quote);
 });
 
 module.exports = app;
