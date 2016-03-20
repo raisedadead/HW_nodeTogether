875fe522ef455773dbbec6c1466f5879a3c28690
Mrugesh Mohapatra <me@raisedadead.com>
2016-03-20 17:31:05 +0530

    Add routes

---
+++ app/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response){
+	response.send("Hello Bengaluru!");
+});
+
+app.get('/helloWorld', function(request, response){
+	response.send("Hello World!");
+});
+
+module.exports = app;
